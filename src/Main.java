45e88fb3640481203d774f973cde9984c3dab9c6
PaulSPuntusov <pps_ru@mail.ru>
2024-10-01 23:34:31 +0300

    не получилось

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IllegalAccessException{
+        Cat cat = new Cat("Vasya",10, new ArrayList<>(Arrays.asList("Anton","Oleg","Igor")));
+        Class catClass = cat.getClass();
+        System.out.println("до Терминатора "+Arrays.toString(catClass.getDeclaredFields()));
+        classFieldTerminator(cat);
+        System.out.println("После Терминатора "+Arrays.toString(catClass.getDeclaredFields()));
+
     }
+    public static void classFieldTerminator(Object object) throws IllegalAccessException {
+        for(Field declaredField : object.getClass().getDeclaredFields()){
+            declaredField.set(object,null);
+
+        }
+    }
+
 }
